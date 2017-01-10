a73f6a0a04cfd626a359719de7dc3d979de282f5
Mateusz Bednarski <mateusz.bednarski@windowslive.com>
2017-01-10 23:10:09 +0100

    Updated evaluator

--- capstone/src/evaluator.py
+++ capstone/src/evaluator.py
@@ -16,31 +16,34 @@ max_episodes = 20000
 
 env = gym.envs.make("LunarLander-v2")
 mon = Monitor(max_episodes)
+env.reset()
 
 agent = LearningAgent(env)
 
 for i_episode in range(max_episodes):
     # print('Episode {}/{} epsilon: {}'.format(i_episode, max_episodes, agent.epsilon))
     observation = env.reset()
-    state = agent.featurize_observation(observation)
+    # state = agent.featurize_observation(observation)
 
     agent.new_episode(i_episode)
 
     mon.observe_episode(i_episode, agent.epsilon)
 
     for t in itertools.count():
-        if t >= 1000:
+        if t >= 10000:
             print('aborting...')
             break
 
-        action = agent.get_action(state)
-        next_observation, reward, done, _ = env.step(env.action_space.sample())
-        next_state = agent.featurize_observation(next_observation)
-        agent.learn(state, next_state, action, reward)
+        action = agent.get_action(observation)
+        next_observation, reward, done, _ = env.step(action)
+        agent.learn(observation, next_observation, action, reward)
 
-        mon.observe_step(i_episode, state, next_state, action, reward)
+        mon.observe_step(i_episode, observation, next_observation, action, reward)
 
-        state = next_state
+        observation = next_observation
+
+        if i_episode % 75 == 0:
+            env.render()
 
         # print('e: {} t: {} r: {} e:{}'.format(i_episode, t, reward, agent.epsilon))
 
@@ -50,7 +53,7 @@ for i_episode in range(max_episodes):
             break
 
 
-mon.save(i_episode)
+# mon.save(i_episode)
 
 p = Plotter()
 p.plot_epsilon()

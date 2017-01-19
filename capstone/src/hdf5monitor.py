3f2c401ef5f509101be69d9ca1ddb1e4bddb7ae4
Mateusz Bednarski <mateusz.bednarski@windowslive.com>
2017-01-19 14:27:20 +0100

    Object version of knnagent

--- capstone/src/hdf5monitor.py
+++ capstone/src/hdf5monitor.py
@@ -1,13 +1,15 @@
 from __future__ import print_function, division
 
 import datetime
+import weakref
+
 import h5py
 import numpy as np
 from util import ensure_dir_exists
 
 
 class Hdf5Monitor(object):
-    def __init__(self, env, agent, observation_size):
+    def __init__(self, env, agent):
         self.env = env
         self.agent = agent
         self.now_string = datetime.datetime.now().strftime('%Y-%m-%d_%H_%M_%S')
@@ -15,9 +17,10 @@ class Hdf5Monitor(object):
         ensure_dir_exists('monitor')
         self.h5file = h5py.File('monitor/' + self.now_string + '.hdf5', 'w')
 
+    def construct(self):
         self.observations = self.h5file.create_dataset('observations',
-                                                       (1, env.observation_space.shape[0]),
-                                                       maxshape=(None, env.observation_space.shape[0]),
+                                                       (1, self.env.observation_space.shape[0]),
+                                                       maxshape=(None, self.env.observation_space.shape[0]),
                                                        chunks=True,
                                                        dtype='f')
         self.observations_count = 0
@@ -31,11 +34,30 @@ class Hdf5Monitor(object):
         self.actions_count = 0
         self.actions_size = 0
 
+
+        self.crewards = self.h5file.create_dataset('crewards',
+                                                  (1,),
+                                                  maxshape=(None,),
+                                                  chunks=True,
+                                                  dtype='f')
+        self.crewards_count = 0
+        self.crewards_size = 0
+
+        # states
+
+        self.states = self.h5file.create_dataset('states',
+                                                  (1,self.agent.get_state_size()),
+                                                  maxshape=(None,self.agent.get_state_size()),
+                                                  chunks=True,
+                                                  dtype='f')
+        self.states_count = 0
+        self.states_size = 0
+
         self.h5file.attrs['created'] = datetime.datetime.now().isoformat()
-        self.h5file.attrs['env'] = env.spec.id
-        self.h5file.attrs['agent'] = agent.name
-        self.h5file.attrs['observation_size'] = env.observation_space.shape[0]
-        self.h5file.attrs.update(agent.get_parameters())
+        self.h5file.attrs['env'] = self.env.spec.id
+        self.h5file.attrs['agent'] = self.agent.name
+        self.h5file.attrs['observation_size'] = self.env.observation_space.shape[0]
+        self.h5file.attrs.update(self.agent.get_parameters())
 
         self.h5file.flush()
 
@@ -52,6 +74,22 @@ class Hdf5Monitor(object):
         self.actions_count += 1
         if not self.actions_count % 2:
             self.h5file.flush()
+
+
+    def append_creward(self, creward):
+        self.crewards.resize((self.crewards_count + 1,))
+        self.crewards[self.crewards_count] = creward
+        self.crewards_count += 1
+        if not self.crewards_count % 2:
+            self.h5file.flush()
+
+    def append_state(self, state):
+        self.states.resize((self.states_count + 1, self.agent.get_state_size()))
+        self.states[self.states_count] = state
+        self.states_count += 1
+        if not self.states_count % 2:
+            self.h5file.flush()
+
 
 
 if __name__ == '__main__':
---
+++ capstone/src/knn_agent.py
@@ -0,0 +1,179 @@
+from __future__ import print_function, division
+
+import random
+
+import numpy as np
+import gym
+from sklearn.neighbors import NearestNeighbors
+
+from hdf5monitor import Hdf5Monitor
+
+
+class BaseAgent(object):
+    def __init__(self, env, name):
+        self.observation_size = env.observation_space.shape[0]
+        self.n_actions = env.action_space.n
+        self.name = name
+        self.env = env
+
+        self.monitor = Hdf5Monitor(env, self)
+
+    def store_episode_stats(self, i_episode, creward):
+        self.monitor.append_creward(creward)
+
+    def store_step_stats(self, observation, state):
+        self.monitor.append_observation(observation)
+        self.monitor.append_state(state)
+
+    def get_parameters(self):
+        return dict()
+
+
+class KNNSARSAAgent(BaseAgent):
+    def __init__(self, env):
+        super(KNNSARSAAgent, self).__init__(env, 'kNN SARSA')
+        self.maxX = np.array([1.0, 2.0, 1.0, 2.0])
+        self.minX = np.array([-1.0, -2.0, -1.0, -2.0])
+
+        self.n_actions = 2
+        self.n_features = 4
+        self.maxsteps = 1000
+        self.maxepisodes = 100
+        self.density = 15
+
+        self.epo = 1
+        self.k = 4
+
+        self.statelist = self.create_clasifiers(self.density)
+        self.nn = NearestNeighbors(n_neighbors=self.k)
+        self.nn.fit(self.statelist)
+
+        self.monitor.construct()
+
+    def get_state_size(self):
+        return self.n_features
+
+    def create_clasifiers(self, density):
+        step = 2 / density
+        frm = -1.
+        to = 1.001
+        xy = np.mgrid[frm:to:step, frm:to:step, frm:to:step, frm:to:step].reshape(self.n_features, -1).T
+        return xy
+
+    def getkNNSet(self, state, statelist, k):
+        # nn = NearestNeighbors(n_neighbors=k)
+        # nn.fit(statelist)
+        d, knn = self.nn.kneighbors([state])
+        d = d[0]
+        knn = knn[0]
+
+        p = np.divide(1.0, (1.0 + np.multiply(d, d)))
+        p = np.divide(p, np.sum(p))
+        return knn, p
+
+    def getValues(self, Q, knn, p):
+        V = Q[knn, :].T.dot(p)
+        return V
+
+    def getBestAction(self, V):
+        a = np.argmax(V)
+        return a
+
+    def e_greedy_selection(self, V, epsilion):
+        action_size = V.shape[0]
+        if random.random() > epsilion:
+            a = self.getBestAction(V)
+        else:
+            a = random.choice(range(action_size))
+        return a
+
+    def update(self, Q, V, V2, knn, p, r, a, ap, trace, alpha, gamma, lambda_, done):
+        trace[knn, :] = 0.0
+        trace[knn, a] = p
+
+        if done:
+            delta = r - V[a]
+        else:
+            delta = (r + np.multiply(gamma, V2[ap])) - V[a]
+
+        Q = Q + alpha * np.multiply(delta, trace)
+        trace = gamma * np.multiply(lambda_, trace)
+        return Q, trace
+
+    def normalize_state(self, state):
+        return 2 * ((state - self.minX) / (self.maxX - self.minX)) - 1
+
+    def episode(self, maxsteps, Q, trace, alpha, gamma, epsilon, lambda_, statelist, actionlist, k, ):
+        state = self.normalize_state(self.env.reset())
+        total_reward = 0.
+        steps = 0
+        knn, p = self.getkNNSet(state, statelist, k)
+        V = self.getValues(Q, knn, p)
+        a = self.e_greedy_selection(V, epsilon)
+
+        for i in range(maxsteps):
+
+            # if not i % 100:
+            #     print('.')
+
+            action = a
+
+            next_observation, reward, done, _ = self.env.step(action)
+            next_state = self.normalize_state(next_observation)
+            # print('{:5}'.format(next_observation))
+
+            total_reward += reward
+
+            knn2, p2 = self.getkNNSet(next_state, statelist, k)
+            V2 = self.getValues(Q, knn2, p2)
+            ap = self.e_greedy_selection(V2, epsilon)
+
+            Q, trace = self.update(Q, V, V2, knn, p, reward, a, ap, trace, alpha, gamma, lambda_, done)
+
+            a = ap
+            state = next_state
+            knn = knn2
+            p = p2
+            V = V2
+
+            steps = steps + 1
+
+            self.store_step_stats(next_observation, state)
+
+            # self.env.render()
+            if done:
+                self.epo += 1
+                print(total_reward, epsilon, self.epo)
+                break
+
+        return total_reward, steps, Q, trace
+
+    def game(self, maxepisodes):
+        actionlist = range(self.n_actions)
+
+
+        n_states = self.statelist.shape[0]
+        Q = np.ones((n_states, self.n_actions)) * 10
+        trace = np.zeros((n_states, self.n_actions))
+        alpha = 0.3
+        gamma = 0.9
+        lambda_ = 0.95
+        epsilon = 1.00
+        k = 4
+
+        for i in range(maxepisodes):
+            # episode
+            total_reward, steps, Q, trace = self.episode(self.maxsteps, Q, trace, alpha, gamma, epsilon, lambda_,
+                                                         self.statelist,
+                                                         actionlist, k)
+
+            self.store_episode_stats(i, total_reward)
+
+            trace.fill(0)
+            epsilon = epsilon * 0.9
+
+
+if __name__ == '__main__':
+    env = gym.envs.make("CartPole-v1")
+    p = KNNSARSAAgent(env)
+    p.game(1300)
